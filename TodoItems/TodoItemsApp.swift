0bf15ac51b10f95fd9e294673f63308425854935
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-04-04 09:15:21 -0400

    Initial Commit

---
+++ TodoItems/TodoItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoItemsApp.swift
+//  TodoItems
+//
+//  Created by Lucas García on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
